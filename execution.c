fdf8480c99eb9d14cad813b7ded474b334a1f4ce
Ivan Kustarev <ivan.kustarev@tune-it.ru>
2024-10-20 23:20:15 +0300

    init signature

---
+++ execution.c
@@ -0,0 +1,18 @@
+//
+// Created by Ivan on 20.10.2024.
+//
+
+#include "execution.h"
+FunExecution *executionGraph(FilenameParseTree *input, int size) {
+  // TODO
+}
+
+// формируем список функций
+int sourceItems(TreeNode *source, TreeNode **sourceItems) {
+  //  TODO
+}
+
+// созадние базовго блока
+ExecutionNode *executionBlock(TreeNode *node, ExecutionNode *baseNode) {
+  //  TODO
+}
---
+++ execution.h
@@ -0,0 +1,39 @@
+//
+// Created by Ivan on 20.10.2024.
+//
+
+#ifndef SPO_LAB1_EXECUTION_H
+#define SPO_LAB1_EXECUTION_H
+
+#include "parser.h"
+
+typedef struct FilenameParseTree FilenameParseTree;
+typedef struct ExecutionNode ExecutionNode;
+typedef struct FunExecution FunExecution;
+
+struct ExecutionNode {
+  char *text;
+  ExecutionNode *definitely; //безусловный переход
+  ExecutionNode *conditionally; //условный переход
+  int childrenNumber;
+  int id;
+};
+
+struct FunExecution {
+  char *name;
+  char *signature;
+  char *filename;
+  int size;
+  ExecutionNode **nodes;
+  char** errors;
+  int errorsCount;
+};
+
+struct FilenameParseTree {
+  char *filename;
+  ParseResult *tree;
+};
+
+FunExecution *executionGraph(FilenameParseTree *input, int size);
+
+#endif // SPO_LAB1_EXECUTION_H
